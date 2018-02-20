c8ffe51d1e6085410021d81687a0bd77340c1722
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-02-20 19:08:08 +0100

    Use printxval_bsearch on evdev_keycode
    * xlat/evdev_keycode.in: Add #unterminated directive at the end and a
    comment about the fact that the xlat is sorted.
    * evdev_xlat.c (print_evdev_keycode): Use printxval_search instead of
    printxval.
    (decode_bitset_): Rename from decode_bitset, add xlat_sorted and
    xlat_size arguments, call printxval_search instead of printxval is
    xlat_sorted is true.
    (decode_bitset): New macro.
    (bit_ioctl, evdev_bitset_ioctl): Pass false as a last argument of
    decode_bitset calls except for evdev_keycode decoding.

--- evdev_xlat.c
+++ evdev_xlat.c
@@ -25,13 +25,17 @@ void print_evdev_ff_type(const kernel_ulong_t val)
 
 void print_evdev_keycode(const unsigned int keycode)
 {
-	printxval(evdev_keycode, keycode, "KEY_???");
+	printxval_search(evdev_keycode, keycode, "KEY_???");
 }
 
+#define decode_bitset(tcp_, arg_, decode_nr_, max_nr_, dflt_, xlat_sorted_) \
+	decode_bitset_((tcp_), (arg_), (decode_nr_), (max_nr_), (dflt_), \
+		(xlat_sorted_), ARRAY_SIZE(decode_nr_))
+
 static int
-decode_bitset(struct tcb *const tcp, const kernel_ulong_t arg,
-	      const struct xlat decode_nr[], const unsigned int max_nr,
-	      const char *const dflt)
+decode_bitset_(struct tcb *const tcp, const kernel_ulong_t arg,
+	       const struct xlat decode_nr[], const unsigned int max_nr,
+	       const char *const dflt, bool xlat_sorted, size_t xlat_size)
 {
 	tprints(", ");
 
@@ -52,7 +56,10 @@ decode_bitset(struct tcb *const tcp, const kernel_ulong_t arg,
 	if (i < 0) {
 		tprints(" 0 ");
 	} else {
-		printxval(decode_nr, i, dflt);
+		if (xlat_sorted)
+			printxval_searchn(decode_nr, xlat_size - 1, i, dflt);
+		else
+			printxval(decode_nr, i, dflt);
 
 		while ((i = next_set_bit(decoded_arg, i + 1, size)) > 0) {
 			if (abbrev(tcp) && bit_displayed >= 3) {
@@ -60,7 +67,13 @@ decode_bitset(struct tcb *const tcp, const kernel_ulong_t arg,
 				break;
 			}
 			tprints(", ");
-			printxval(decode_nr, i, dflt);
+
+			if (xlat_sorted)
+				printxval_searchn(decode_nr, xlat_size - 1, i,
+						  dflt);
+			else
+				printxval(decode_nr, i, dflt);
+
 			bit_displayed++;
 		}
 	}
@@ -77,43 +90,44 @@ bit_ioctl(struct tcb *const tcp, const unsigned int ev_nr,
 	switch (ev_nr) {
 		case EV_SYN:
 			return decode_bitset(tcp, arg, evdev_sync,
-					     SYN_MAX, "SYN_???");
+					     SYN_MAX, "SYN_???", false);
 		case EV_KEY:
 			return decode_bitset(tcp, arg, evdev_keycode,
-					     KEY_MAX, "KEY_???");
+					     KEY_MAX, "KEY_???", true);
 		case EV_REL:
 			return decode_bitset(tcp, arg, evdev_relative_axes,
-					     REL_MAX, "REL_???");
+					     REL_MAX, "REL_???", false);
 		case EV_ABS:
 			return decode_bitset(tcp, arg, evdev_abs,
-					     ABS_MAX, "ABS_???");
+					     ABS_MAX, "ABS_???", false);
 		case EV_MSC:
 			return decode_bitset(tcp, arg, evdev_misc,
-					     MSC_MAX, "MSC_???");
+					     MSC_MAX, "MSC_???", false);
 # ifdef EV_SW
 		case EV_SW:
 			return decode_bitset(tcp, arg, evdev_switch,
-					     SW_MAX, "SW_???");
+					     SW_MAX, "SW_???", false);
 # endif
 		case EV_LED:
 			return decode_bitset(tcp, arg, evdev_leds,
-					     LED_MAX, "LED_???");
+					     LED_MAX, "LED_???", false);
 		case EV_SND:
 			return decode_bitset(tcp, arg, evdev_snd,
-					     SND_MAX, "SND_???");
+					     SND_MAX, "SND_???", false);
 		case EV_REP:
 			return decode_bitset(tcp, arg, evdev_autorepeat,
-					     REP_MAX, "REP_???");
+					     REP_MAX, "REP_???", false);
 		case EV_FF:
 			return decode_bitset(tcp, arg, evdev_ff_types,
-					     FF_MAX, "FF_???");
+					     FF_MAX, "FF_???", false);
 		case EV_PWR:
 			tprints(", ");
 			printnum_int(tcp, arg, "%d");
 			return RVAL_IOCTL_DECODED;
 		case EV_FF_STATUS:
 			return decode_bitset(tcp, arg, evdev_ff_status,
-					     FF_STATUS_MAX, "FF_STATUS_???");
+					     FF_STATUS_MAX, "FF_STATUS_???",
+					     false);
 		default:
 			tprints(", ");
 			printaddr(arg);
@@ -129,22 +143,22 @@ evdev_bitset_ioctl(struct tcb * const tcp, const unsigned int code,
 # ifdef EVIOCGPROP
 		case _IOC_NR(EVIOCGPROP(0)):
 			return decode_bitset(tcp, arg, evdev_prop,
-					     INPUT_PROP_MAX, "PROP_???");
+					     INPUT_PROP_MAX, "PROP_???", false);
 # endif
 		case _IOC_NR(EVIOCGSND(0)):
 			return decode_bitset(tcp, arg, evdev_snd,
-					     SND_MAX, "SND_???");
+					     SND_MAX, "SND_???", false);
 # ifdef EVIOCGSW
 		case _IOC_NR(EVIOCGSW(0)):
 			return decode_bitset(tcp, arg, evdev_switch,
-					     SW_MAX, "SW_???");
+					     SW_MAX, "SW_???", false);
 # endif
 		case _IOC_NR(EVIOCGKEY(0)):
 			return decode_bitset(tcp, arg, evdev_keycode,
-					     KEY_MAX, "KEY_???");
+					     KEY_MAX, "KEY_???", true);
 		case _IOC_NR(EVIOCGLED(0)):
 			return decode_bitset(tcp, arg, evdev_leds,
-					     LED_MAX, "LED_???");
+					     LED_MAX, "LED_???", false);
 	}
 
 	error_func_msg("Unexpected code: %u", code);
