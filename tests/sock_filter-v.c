34586c77853724014d9964c87b4a72411fcf32d4
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-04-06 15:39:32 +0200

    Add variants of sock_filtrer-v test with different xlat verbosity levels
    * tests/.gitignore: Add sock_filter-v-Xabbrev, sock_filter-v-Xraw,
    and sock_filter-v-Xverbose.
    * tests/pure_executables.list: Likewise
    * tests/gen_tests.in (sock_filter-v-Xabbrev, sock_filter-v-Xraw,
    sock_filter-v-Xverbose): New test.
    * tests/sock_filter-v-Xabbrev.c: New file.
    * tests/sock_filter-v-Xraw.c: Likewise.
    * tests/sock_filter-v-Xverbose.c: Likewise.
    * tests/sock_filter-v.c (PRINT_STMT_SYM, PRINT_STMT_VAL, PRINT_JUMP): Do not
    stringify code and k, print them directly.
    (PRINT_STMT_VAL_, PRINT_JUMP_): Remove.
    [SOL_SOCKET]: Check that SOL_SOCKET equals to 1, undefine.
    [SO_ATTACH_FILTER]: Check that SO_ATTACH_FILTER equals to 0x1a, undefine.
    [SO_ATTACH_REUSEPORT_CBPF]: Check that SO_ATTACH_REUSEPORT_CBPF equals to 0x33,
    undefine.
    [BPF_LD]: Check that BPF_LD equals to 0, undefine.
    [BPF_K]: Check that BPF_K equals to 0, undefine.
    [BPF_W]: Check that BPF_W equals to 0, undefine.
    [BPF_JMP]: Check that BPF_JMP equals to 5, undefine.
    [BPF_RET]: Check that BPF_RET equals to 6, undefine.
    [SKF_LL_OFF]: Check that SKF_LL_OFF equals to -0x200000, undefine.
    [SKF_NET_OFF]: Check that SKF_NET_OFF equals to -0x100000, undefine.
    [SKF_AD_OFF]: Check that SKF_AD_OFF equals to -0x1000, undefine.
    (SOL_SOCKET, SO_ATTACH_FILTER, SO_ATTACH_REUSEPORT_CBPF, BPF_LD, BPF_K,
    BPF_W, BPF_JMP, BPF_RET, SKF_LL_OFF, SKF_NET_OFF, SKF_AD_OFF): New macro
    constant.
    (STR, STR1, STR2, STR3): New macro.
    (print_filter): Wrap symbolic constant in STR* macros, wrap the rest in
    STR.
    (main): Print SOL_SOCKET, SO_ATTACH_FILTER, and SO_ATTACH_REUSEPORT_CBPF
    using STR1 macro.

--- tests/sock_filter-v.c
+++ tests/sock_filter-v.c
@@ -35,15 +35,137 @@
 #include <sys/socket.h>
 #include <linux/filter.h>
 
-#define PRINT_STMT_SYM(pfx, code, k)	PRINT_STMT_SYM_(pfx, #code, #k)
-#define PRINT_STMT_SYM_(pfx, code, k)	\
+#if defined __alpha__ || defined __hppa__ || defined __mips__ \
+	|| defined __sparc__
+# define SOL_SOCKET_EXPECTED 0xffff
+#else
+# define SOL_SOCKET_EXPECTED 0x1
+#endif
+
+#ifdef SOL_SOCKET
+# if SOL_SOCKET != SOL_SOCKET_EXPECTED
+#  error "Unexpected value of SOL_SOCKET"
+# endif
+# undef SOL_SOCKET
+#endif /* SOL_SOCKET */
+#define SOL_SOCKET SOL_SOCKET_EXPECTED
+
+#if defined __hppa__
+# define SO_ATTACH_FILTER_EXPECTED 0x401a
+#else
+# define SO_ATTACH_FILTER_EXPECTED 0x1a
+#endif
+
+#ifdef SO_ATTACH_FILTER
+# if SO_ATTACH_FILTER != SO_ATTACH_FILTER_EXPECTED
+#  error "Unexpected value of SO_ATTACH_FILTER"
+# endif
+# undef SO_ATTACH_FILTER
+#endif /* SO_ATTACH_FILTER */
+#define SO_ATTACH_FILTER SO_ATTACH_FILTER_EXPECTED
+
+#if defined __hppa__
+# define SO_ATTACH_REUSEPORT_CBPF_EXPECTED 0x402c
+#elif defined __sparc__
+# define SO_ATTACH_REUSEPORT_CBPF_EXPECTED 0x35
+#else
+# define SO_ATTACH_REUSEPORT_CBPF_EXPECTED 0x33
+#endif
+
+#ifdef SO_ATTACH_REUSEPORT_CBPF
+# if SO_ATTACH_REUSEPORT_CBPF != SO_ATTACH_REUSEPORT_CBPF_EXPECTED
+#  error "Unexpected value of SO_ATTACH_REUSEPORT_CBPF"
+# endif
+# undef SO_ATTACH_REUSEPORT_CBPF
+# define SO_ATTACH_REUSEPORT_CBPF SO_ATTACH_REUSEPORT_CBPF_EXPECTED
+#endif /* SO_ATTACH_REUSEPORT_CBPF */
+
+#ifdef BPF_LD
+# if BPF_LD != 0x00
+#  error "Unexpected value of BPF_LD"
+# endif
+# undef BPF_LD
+#endif /* BPF_LD */
+#define BPF_LD 0
+
+#ifdef BPF_K
+# if BPF_K != 0x00
+#  error "Unexpected value of BPF_K"
+# endif
+# undef BPF_K
+#endif /* BPF_K */
+#define BPF_K 0
+
+#ifdef BPF_W
+# if BPF_W != 0x00
+#  error "Unexpected value of BPF_W"
+# endif
+# undef BPF_W
+#endif /* BPF_W */
+#define BPF_W 0
+
+#ifdef BPF_JMP
+# if BPF_JMP != 0x05
+#  error "Unexpected value of BPF_JMP"
+# endif
+# undef BPF_JMP
+#endif /* BPF_JMP */
+#define BPF_JMP 0x5
+
+#ifdef BPF_RET
+# if BPF_RET != 0x06
+#  error "Unexpected value of BPF_RET"
+# endif
+# undef BPF_RET
+#endif /* BPF_RET */
+#define BPF_RET 0x6
+
+#ifdef SKF_LL_OFF
+# if SKF_LL_OFF != -0x200000
+#  error "Unexpected value of SKF_LL_OFF"
+# endif
+# undef SKF_LL_OFF
+#endif /* SKF_LL_OFF */
+#define SKF_LL_OFF 0xffe00000
+
+#ifdef SKF_NET_OFF
+# if SKF_NET_OFF != -0x100000
+#  error "Unexpected value of SKF_NET_OFF"
+# endif
+# undef SKF_NET_OFF
+#endif /* SKF_NET_OFF */
+#define SKF_NET_OFF 0xfff00000
+
+#ifdef SKF_AD_OFF
+# if SKF_AD_OFF != -0x1000
+#  error "Unexpected value of SKF_AD_OFF"
+# endif
+# undef SKF_AD_OFF
+#endif /* SKF_AD_OFF */
+#define SKF_AD_OFF 0xfffff000
+
+#define STR(a_) #a_
+#if XLAT_RAW
+# define STR1(a_) STR(a_)
+# define STR2(a_, b_) STR(a_) "|" STR(b_)
+# define STR3(a_, b_, c_) STR(a_) "|" STR(b_) "|" STR(c_)
+#elif XLAT_VERBOSE
+# define STR1(a_) STR(a_) " /* " #a_ " */"
+# define STR2(a_, b_) STR(a_) " /* " #a_ " */|" STR(b_) " /* " #b_ " */"
+# define STR3(a_, b_, c_) \
+	STR(a_) " /* " #a_ " */|" STR(b_) " /* " #b_ " */|" STR(c_) " /* " #c_ " */"
+#else
+# define STR1(a_) #a_
+# define STR2(a_, b_) #a_ "|" #b_
+# define STR3(a_, b_, c_) #a_ "|" #b_ "|" #c_
+#endif
+
+#define PRINT_STMT_SYM(pfx, code, k)	\
 	printf("%sBPF_STMT(%s, %s)", pfx, code, k)
-#define PRINT_STMT_VAL(pfx, code, k)	PRINT_STMT_VAL_(pfx, #code, k)
-#define PRINT_STMT_VAL_(pfx, code, k)	\
+#define PRINT_STMT_VAL(pfx, code, k)	\
 	printf("%sBPF_STMT(%s, %#x)", pfx, code, k)
 
-#define PRINT_JUMP(pfx, code, k, jt, jf)	PRINT_JUMP_(pfx, #code, k, jt, jf)
-#define PRINT_JUMP_(pfx, code, k, jt, jf)	\
+#define PRINT_JUMP(pfx, code, k, jt, jf) \
 	printf("%sBPF_JUMP(%s, %#x, %#x, %#x)", pfx, code, k, jt, jf)
 
 static const struct sock_filter bpf_filter[] = {
@@ -62,16 +184,19 @@ static const struct sock_filter bpf_filter[] = {
 static void
 print_filter(void)
 {
-	PRINT_STMT_SYM("[", BPF_LD|BPF_B|BPF_ABS, SKF_LL_OFF+4);
-	PRINT_STMT_SYM(", ", BPF_LD|BPF_B|BPF_ABS, SKF_NET_OFF+8);
-	PRINT_STMT_SYM(", ", BPF_LD|BPF_B|BPF_ABS, SKF_AD_OFF+SKF_AD_PROTOCOL);
-	PRINT_JUMP(", ", BPF_JMP|BPF_K|BPF_JEQ, IPPROTO_UDP, 0, 5);
-	PRINT_STMT_VAL(", ", BPF_LD|BPF_W|BPF_LEN, 0);
-	PRINT_JUMP(", ", BPF_JMP|BPF_K|BPF_JGE, 100, 0, 3);
-	PRINT_STMT_VAL(", ", BPF_LD|BPF_B|BPF_ABS, 42);
-	PRINT_JUMP(", ", BPF_JMP|BPF_K|BPF_JEQ, 'a', 0, 1);
-	PRINT_STMT_VAL(", ", BPF_RET|BPF_K, -1U);
-	PRINT_STMT_VAL(", ", BPF_RET|BPF_K, 0);
+	PRINT_STMT_SYM("[", STR3(BPF_LD, BPF_B, BPF_ABS),
+		       STR1(SKF_LL_OFF) STR(+4));
+	PRINT_STMT_SYM(", ", STR3(BPF_LD, BPF_B, BPF_ABS),
+		       STR1(SKF_NET_OFF) STR(+8));
+	PRINT_STMT_SYM(", ", STR3(BPF_LD, BPF_B, BPF_ABS),
+		       STR1(SKF_AD_OFF) STR(+) STR1(SKF_AD_PROTOCOL));
+	PRINT_JUMP(", ", STR3(BPF_JMP, BPF_K, BPF_JEQ), IPPROTO_UDP, 0, 5);
+	PRINT_STMT_VAL(", ", STR3(BPF_LD, BPF_W, BPF_LEN), 0);
+	PRINT_JUMP(", ", STR3(BPF_JMP, BPF_K, BPF_JGE), 100, 0, 3);
+	PRINT_STMT_VAL(", ", STR3(BPF_LD, BPF_B, BPF_ABS), 42);
+	PRINT_JUMP(", ", STR3(BPF_JMP, BPF_K, BPF_JEQ), 'a', 0, 1);
+	PRINT_STMT_VAL(", ", STR2(BPF_RET, BPF_K), -1U);
+	PRINT_STMT_VAL(", ", STR2(BPF_RET, BPF_K), 0);
 	putchar(']');
 }
 
@@ -115,34 +240,35 @@ main(void)
 	rc = get_filter(fd, NULL, len);
 	if (rc)
 		perror_msg_and_skip("getsockopt SOL_SOCKET SO_ATTACH_FILTER");
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER"
-	       ", NULL, [%u->0]) = 0\n", fd, BPF_MAXINSNS);
+	printf("getsockopt(%d, %s, %s, NULL, [%u->0]) = 0\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), BPF_MAXINSNS);
 
 	/* getsockopt NULL optlen - EFAULT */
 	rc = get_filter(fd, NULL, NULL);
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, NULL, NULL)"
-	       " = %s\n", fd, errstr);
+	printf("getsockopt(%d, %s, %s, NULL, NULL) = %s\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), errstr);
 
 	/* attach a filter */
 	rc = set_filter(fd, prog, sizeof(*prog));
 	if (rc)
 		perror_msg_and_skip("setsockopt SOL_SOCKET SO_ATTACH_FILTER");
-	printf("setsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, {len=%u, filter=",
-	       fd, prog->len);
+	printf("setsockopt(%d, %s, %s, {len=%u, filter=",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), prog->len);
 	print_filter();
 	printf("}, %u) = 0\n", (unsigned int) sizeof(*prog));
 
 	/* setsockopt optlen is too small - EINVAL */
 	rc = set_filter(fd, prog, sizeof(*prog) - 4);
-	printf("setsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, %p, %u) = %s\n",
-	       fd, prog, (unsigned int) sizeof(*prog) - 4, errstr);
+	printf("setsockopt(%d, %s, %s, %p, %u) = %s\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), prog,
+	       (unsigned int) sizeof(*prog) - 4, errstr);
 
 #ifdef SO_ATTACH_REUSEPORT_CBPF
 	rc = setsockopt(fd, SOL_SOCKET, SO_ATTACH_REUSEPORT_CBPF,
 			prog, sizeof(*prog));
 	errstr = sprintrc(rc);
-	printf("setsockopt(%d, SOL_SOCKET, SO_ATTACH_REUSEPORT_CBPF"
-	       ", {len=%u, filter=", fd, prog->len);
+	printf("setsockopt(%d, %s, %s, {len=%u, filter=",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_REUSEPORT_CBPF), prog->len);
 	print_filter();
 	printf("}, %u) = %s\n", (unsigned int) sizeof(*prog), errstr);
 #endif
@@ -150,28 +276,29 @@ main(void)
 	/* query sock_filter program length -> ARRAY_SIZE(bpf_filter) */
 	*len = 0;
 	rc = get_filter(fd, efault, len);
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, %p"
-	       ", [0->%u]) = %s\n",
-	       fd, efault, (unsigned int) ARRAY_SIZE(bpf_filter), errstr);
+	printf("getsockopt(%d, %s, %s, %p, [0->%u]) = %s\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), efault,
+	       (unsigned int) ARRAY_SIZE(bpf_filter), errstr);
 
 	/* getsockopt optlen is too small - EINVAL */
 	*len = ARRAY_SIZE(bpf_filter) - 1;
 	rc = get_filter(fd, efault, len);
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, %p"
-	       ", [%u]) = %s\n",
-	       fd, efault, (unsigned int) ARRAY_SIZE(bpf_filter) - 1, errstr);
+	printf("getsockopt(%d, %s, %s, %p, [%u]) = %s\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), efault,
+	       (unsigned int) ARRAY_SIZE(bpf_filter) - 1, errstr);
 
 	/* getsockopt optval EFAULT */
 	*len = ARRAY_SIZE(bpf_filter);
 	rc = get_filter(fd, filter + 1, len);
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, %p"
-	       ", [%u]) = %s\n", fd, filter + 1,
+	printf("getsockopt(%d, %s, %s, %p, [%u]) = %s\n",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER), filter + 1,
 	       (unsigned int) ARRAY_SIZE(bpf_filter), errstr);
 
 	/* getsockopt optlen is too large - truncated */
 	*len = ARRAY_SIZE(bpf_filter) + 1;
 	rc = get_filter(fd, filter, len);
-	printf("getsockopt(%d, SOL_SOCKET, SO_ATTACH_FILTER, ", fd);
+	printf("getsockopt(%d, %s, %s, ",
+	       fd, STR1(SOL_SOCKET), STR1(SO_ATTACH_FILTER));
 	print_filter();
 	printf(", [%u->%d]) = %s\n",
 	       (unsigned int) ARRAY_SIZE(bpf_filter) + 1, *len, errstr);
