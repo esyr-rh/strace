7df3cbd6d0abc58ad44100fe4c1a464edbde2314
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-04-11 17:28:50 +0200

    net.c: decode SOL_CAIF socket option names
    * xlat/sockcaifoptions.in: New file.
    * net.c (print_sockopt_fd_level_name) <case SOL_CAIF>: Decode socket
    option names.

--- net.c
+++ net.c
@@ -440,6 +440,7 @@ SYS_FUNC(socketpair)
 #include "xlat/socksctpoptions.h"
 #include "xlat/socktcpoptions.h"
 #include "xlat/sockudpoptions.h"
+#include "xlat/sockcaifoptions.h"
 #include "xlat/sockalgoptions.h"
 #include "xlat/socknfcllcpoptions.h"
 #include "xlat/sockkcmoptions.h"
@@ -486,6 +487,9 @@ print_sockopt_fd_level_name(struct tcb *tcp, int fd, unsigned int level,
 		break;
 	case SOL_UDP:
 		printxval(sockudpoptions, name, "UDP_???");
+		break;
+	case SOL_CAIF:
+		printxval(sockcaifoptions, name, "CAIFSO_???");
 		break;
 	case SOL_ALG:
 		printxval_index(sockalgoptions, name, "ALG_???");
