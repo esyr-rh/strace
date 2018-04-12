b45da3d0536ebb3cb17b90f7a91e73eaee5c8521
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-04-12 11:11:44 +0200

    net.c: decode SOL_TIPC socket option names
    * xlat/socktipcoptions.in: New file.
    * net.c (print_sockopt_fd_level_name) <case SOL_TIPC>: Decode socket
    option names.

--- net.c
+++ net.c
@@ -467,6 +467,7 @@ SYS_FUNC(socketpair)
 #include "xlat/socksctpoptions.h"
 #include "xlat/socktcpoptions.h"
 #include "xlat/sockudpoptions.h"
+#include "xlat/socktipcoptions.h"
 #include "xlat/sockrxrpcoptions.h"
 #include "xlat/sockpppol2tpoptions.h"
 #include "xlat/sockbluetoothoptions.h"
@@ -520,6 +521,9 @@ print_sockopt_fd_level_name(struct tcb *tcp, int fd, unsigned int level,
 		break;
 	case SOL_UDP:
 		printxval(sockudpoptions, name, "UDP_???");
+		break;
+	case SOL_TIPC:
+		printxval_search(socktipcoptions, name, "TIPC_???");
 		break;
 	case SOL_RXRPC:
 		printxval_index(sockrxrpcoptions, name, "RXRPC_???");
